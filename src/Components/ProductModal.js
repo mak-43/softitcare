b56191a9d6dd4614807deab93c266126c730b521
Mak-43 <letsmailmak@gmail.com>
2022-08-01 17:08:19 +0600

    delete modal

---
+++ src/Components/ProductModal.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+
+const ProductModal = ({ id, refetch, setId }) => {
+
+    const handleproduct = id => {
+        fetch(`https://morning-atoll-82384.herokuapp.com/product/${id}`, {
+            method: 'Delete',
+        }).then(res => res.json())
+            .then(data => {
+
+                toast.success('Product Deleted')
+
+                setId(null)
+                refetch()
+
+            })
+    }
+    return (
+        <div>
+            <input type="checkbox" id="product-modal" class="modal-toggle" />
+            <div class="modal modal-bottom sm:modal-middle">
+                <div class="modal-box">
+
+                    <h3 class="font-bold text-lg">Do you want to Delete This Product?</h3>
+
+                    <div class="modal-action">
+                        <label onClick={() => handleproduct(id)} for="product-modal" class="btn">Yes</label>
+                        <label for="product-modal" class="btn ">No</label>
+                    </div>
+                </div>
+            </div>
+        </div >
+    );
+};
+
+export default ProductModal;
--- src/Pages/AddProduct.js
+++ src/Pages/AddProduct.js
@@ -3,12 +3,13 @@ import React, { useState } from 'react';
 
 import AddModal from '../Components/AddModal';
 import Loading from '../Components/Loading';
+import ProductModal from '../Components/ProductModal';
 
 const AddProduct = () => {
     const [add,setAdd]=useState(null)
-  
+    const [id, setId] = useState(null)
     
-    const { isLoading, error, data, refetch } = useQuery(['products'], () => fetch('http://localhost:5000/products').then(res => res.json()))
+    const { isLoading, error, data:products, refetch } = useQuery(['products'], () => fetch('http://localhost:5000/products').then(res => res.json()))
 
     if(isLoading){
         return <Loading/>
@@ -16,11 +17,57 @@ const AddProduct = () => {
 
     return (
         <div className='h-screen'>
-            <div className='flex justify-center items-center'>
+            <div className='flex flex-col justify-center items-center'>
                 <label onClick={()=>setAdd(true)} for="addmodal" class="btn my-5">Add A Product</label>
-               
+                <div class="overflow-x-auto">
+                <table class="table w-full">
+
+                    <thead>
+                        <tr>
+                            <th>SL</th>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th>Minimum Quantity</th>
+                            <th>Available Quantity</th>
+                            <th>Price</th>
+                            <th>Cancel</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+
+                        {
+                            products?.map((o, index) =>
+                                <tr>
+                                    <th>{index + 1}</th>
+                                    <td><div class="avatar">
+                                        <div class="mask mask-squircle w-12 h-12">
+                                            <img src={o?.img} alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div></td>
+                                    <td>{o?.name}</td>
+                                    <td>{o?.description}</td>
+                                    <td>{o?.minimum}</td>
+                                    <td>{o?.available}</td>
+                                    <td>$ {o?.price}</td>
+
+
+                                    <label onClick={() => setId(`${o?._id}`)} for="product-modal" class=" cursor-pointer text-red-500 text-xl"><i class="fa-solid fa-delete-left mt-6 ml-5"></i></label>
+
+
+                                </tr>
+                            )
+                        }
+
+                    </tbody>
+                </table>
+            </div>
 
             </div>
+            {
+                 id && <ProductModal id={id} setId={setId} refetch={refetch} ></ProductModal>
+            }
             {
                 add && <AddModal setAdd={setAdd} refetch={refetch}></AddModal>
             }
